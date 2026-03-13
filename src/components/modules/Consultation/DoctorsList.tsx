da85a0a15d0d9e22d0b0ffcb996826ac40175cac
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-03-14 03:36:23 +0600

    Adding axios,tanstack query and fetching data

---
+++ src/components/modules/Consultation/DoctorsList.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { getDoctors } from "@/app/(commonLayout)/consultation/_actions";
+import { useQuery } from "@tanstack/react-query";
+
+const DoctorsList = () => {
+     const { data : doctorData } = useQuery({
+       queryKey: ["doctors"],
+       queryFn: () => getDoctors(),
+     });
+
+
+     //non-prefetched query example
+    //  const {data : nonPrefetchedData} = useQuery({
+    //    queryKey: ["doctors-non-prefetched"],
+    //    queryFn: () => getDoctors(),
+    //  });
+
+
+  return (
+    <div>{doctorData!.data.map((doctor: any) => (
+      <div key={doctor.id}>{doctor.name}</div>
+    ))}</div>
+  )
+}
+
+export default DoctorsList
